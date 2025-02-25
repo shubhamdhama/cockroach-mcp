bd37bca968d303fbaf953e2a81ca535453cc97e1
Shubham Dhama <shubham.dhama@cockroachlabs.com>
2025-02-25 09:46:13 +0530

    env: update `.env` file location

--- cmd/cockroach-mcp/main.go
+++ cmd/cockroach-mcp/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
 	"github.com/shubhamdhama/cockroach-mcp/pkg/db"
@@ -9,7 +11,12 @@ import (
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	envPath := filepath.Join(home, ".mcp-env")
+	if err := godotenv.Load(envPath); err != nil {
 		log.Println("Warning: No .env file found")
 	}
 
